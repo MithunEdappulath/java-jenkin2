f000bc125d79eebe7f5686999ea03cbaa82e5516
MithunEdappulath <120838449+MithunEdappulath@users.noreply.github.com>
2023-01-15 16:24:16 -0500

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 class Simple{
     public static void main(String args[])
 {
-    System.out.println( "This is my Second Jenkins Project and automatic setup");
+    System.out.println( "This is my Second Jenkins Project and automatic setup 2");
 }
 }
